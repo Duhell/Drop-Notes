fa8416bbd22c58126e0d145e590ac7d6c8786f67
Duhell <ryanvillarma148@gmail.com>
2022-08-04 01:06:52 +0800

    Update render.js

--- server/services/render.js
+++ server/services/render.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 exports.home = (req,res)=>{
-	axios.get("http://localhost:4000/api/note")
+	axios.get("https://dropsnote.herokuapp.com/api/note")
 	.then(function(response){
 		res.render('index',{writings: response.data})
 		console.log(response.data)
@@ -16,7 +16,7 @@ exports.add_note = (req,res)=>{
 }
 
 exports.update_note = (req,res)=>{
-	axios.get("http://localhost:4000/api/note", {params:{id:req.query.id}})
+	axios.get("https://dropsnote.herokuapp.com/api/note", {params:{id:req.query.id}})
 	.then(function(notedata){
 		res.render('_edit',{note: notedata.data} )
 
