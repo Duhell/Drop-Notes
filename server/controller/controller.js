898f388dd46a91647c01daf04a6c5c7181dcdda7
Duhell <ryanvillarma148@gmail.com>
2022-08-04 10:42:24 +0800

    Update controller.js

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,5 +1,4 @@
 let Notedb = require('../model/model')
-
 //create new note
 
 exports.create = (req,res)=>{
@@ -12,7 +11,7 @@ exports.create = (req,res)=>{
 	const note = new Notedb({
 		title: req.body.title,
 		text: req.body.text,
-		time: `${new Date().getMonth()+1}-${new Date().getDate()}-${new Date().getFullYear()}`
+		time: new Date().toLocaleDateString("en-US", {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'} )
 	})
 	note.save(note)
 		.then(data=>{
